4db878dbb04fecd976ba183df7b33333f503da7d
ebnsina <ebnsina.me@gmail.com>
2020-07-19 20:41:59 +0600

    mobile tweak

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Card = (props) => {
   const { name, username, email, website } = props.user;
   return (
-    <div className="flex flex-col items-center justify-center bg-white p-4 shadow mb-3 md:mb-0">
+    <div className="flex flex-col items-center justify-center bg-white p-4 shadow mb-2 md:mb-0">
       <h1 className="text-xl font-bold">{name}</h1>
       <small className="text-sm text-gray-700">({username})</small>
       <p>
