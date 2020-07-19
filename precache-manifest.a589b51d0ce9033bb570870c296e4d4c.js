3d1cc493e84f9f7a7fbd484d16928bbe724f597d
ebnsina <ebnsina.me@gmail.com>
2020-07-19 18:23:56 +0600

    Updates

---
+++ precache-manifest.a589b51d0ce9033bb570870c296e4d4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6eb98b061c657152cd361eb62bfdc16",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "ba9eb580c63a6030bfdd",
+    "url": "/react-app/static/css/main.37dbfe78.chunk.css"
+  },
+  {
+    "revision": "eb1d9a75c88570f4e746",
+    "url": "/react-app/static/js/2.feaeb0f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.feaeb0f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9eb580c63a6030bfdd",
+    "url": "/react-app/static/js/main.2f054821.chunk.js"
+  },
+  {
+    "revision": "ac872109be052ea36539",
+    "url": "/react-app/static/js/runtime-main.bdfca5b3.js"
+  }
+]);
